04ef53003479628f60f8a00f97e6ede046d3b69f
Hyunil Yoo <hyunilg@gmail.com>
2025-05-13 23:42:04 -0700

    update

--- kg-predict.sh
+++
@@ -1,8 +0,0 @@
-export TEST_DATA='/Users/hyunilyoo/project/kaggle-com/monthly/data/test.csv'
-export MODEL='xgboost_reg'
-export MODEL_PATH='/Users/hyunilyoo/project/kaggle-com/monthly/model/'
-export VERSION='v1'
-export TOTAL_FOLD=10
-export OUTPUT_PATH='/Users/hyunilyoo/project/kaggle-com/monthly/output/predictions.csv'
-
-python predict_main.py 
